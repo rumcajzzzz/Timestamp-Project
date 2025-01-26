b7130345344f180e9a4d5bb857c4b4b52e38c44c
Marcel Niemiec <marcel.niemiec6@gmail.com>
2025-01-26 18:13:22 +0100

    feat: implemented date function

--- index.js
+++ index.js
@@ -10,8 +10,34 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", (req, res) => {
+
+  var dateParams = req.params.date;
+
+  if(!dateParams) {
+    var currentDate = new Date();
+    res.json({
+      unix: currentDate.getTime(),
+      utc: currentDate.toUTCString()
+    });
+    return;
+  }
+
+  if(!isNaN(dateParams)) { 
+    dateParams = parseInt(dateParams);
+  }
+
+  let date = new Date(dateParams);
+
+  if(date.toString() === "Invalid Date") {
+    res.json({error: "Invalid Date"});
+  } else {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  }
+
 });
 
 var listener = app.listen(process.env.PORT || 3000, function () {
